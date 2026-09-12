820ea8e99ecec537e10825c4da4839e42792aba0
yashin-sh <yacinesediri@gmail.com>
2026-09-12 21:23:35 +0200

    feat: queue deferred NAND callbacks

--- source/switch_nand_callback_runtime.cpp
+++ source/switch_nand_callback_runtime.cpp
@@ -2,9 +2,37 @@
 
 #include "abi_bridge.h"
 
-namespace mkw::switch_nand_runtime {
+#include <deque>
+#include <mutex>
 
-void QueueCallback(std::uint32_t, std::int32_t, std::uint32_t) noexcept {}
+namespace mkw::switch_nand_runtime {
+namespace {
+
+struct PendingCallback {
+    std::uint32_t callbackPtr = 0u;
+    std::int32_t result = 0;
+    std::uint32_t commandBlockPtr = 0u;
+};
+
+std::mutex g_callbackMutex;
+std::deque<PendingCallback> g_pendingCallbacks;
+thread_local int g_callbackDrainDepth = 0;
+
+} // namespace
+
+void QueueCallback(std::uint32_t callbackPtr,
+                   std::int32_t result,
+                   std::uint32_t commandBlockPtr) noexcept {
+    if (callbackPtr == 0u) {
+        return;
+    }
+    try {
+        std::lock_guard<std::mutex> lock(g_callbackMutex);
+        g_pendingCallbacks.push_back({callbackPtr, result, commandBlockPtr});
+    } catch (...) {
+    }
+}
+
 void PumpCallbacks(CpuContext*) noexcept {}
 
 } // namespace mkw::switch_nand_runtime

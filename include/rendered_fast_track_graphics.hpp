0f2f9ff877d5c75d0e9023dc2f3db88115bd929b
yashin-sh <yacinesediri@gmail.com>
2026-09-19 12:12:35 +0200

    diag: expose RMCP01 FIFO command shape

--- include/rendered_fast_track_graphics.hpp
+++ include/rendered_fast_track_graphics.hpp
@@ -9,6 +9,17 @@ struct MkwSwitchRendererDiagnostics {
     bool frame_active = false;
     bool fifo_work_seen = false;
     std::uint64_t fifo_write_calls = 0;
+    std::uint64_t fifo_write8_calls = 0;
+    std::uint64_t fifo_write16_calls = 0;
+    std::uint64_t fifo_write32_calls = 0;
+    std::uint64_t fifo_write_float_calls = 0;
+    std::uint64_t bp_reg_49_calls = 0;
+    std::uint64_t bp_reg_4a_calls = 0;
+    std::uint64_t bp_reg_4d_calls = 0;
+    std::uint64_t bp_reg_other_calls = 0;
+    std::uint32_t last_fifo_value = 0;
+    std::uint32_t last_bp_word = 0;
+    std::uint8_t last_fifo_size = 0;
     std::uint64_t display_list_calls = 0;
     std::uint64_t gx_copy_disp_calls = 0;
     std::uint64_t present_successes = 0;

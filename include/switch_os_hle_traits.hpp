408ad0a566c0496d1ab76c4715507f12a170cff5
yashin-sh <yacinesediri@gmail.com>
2026-09-19 15:12:43 +0200

    m5: register local DVD read overrides

--- include/switch_os_hle_traits.hpp
+++ include/switch_os_hle_traits.hpp
@@ -186,5 +186,6 @@ struct KnownNativeCpuCall<0x801B1BE4u> {
 #include "switch_nand_read_async_hle_traits.hpp"
 #include "switch_nand_close_async_hle_traits.hpp"
 #include "switch_dvd_hle_traits.hpp"
+#include "switch_dvd_read_hle_traits.hpp"
 #include "switch_dvd_low_hle_traits.hpp"
 #include "switch_esp_hle_traits.hpp"
